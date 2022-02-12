e2308526d355571020ca74c8aa4cca99a2c33a48
Brandon Ng <nwjbrandon@gmail.com>
2022-02-12 10:17:39 +0800

    feat: train 3d pose model (#12)

--- demo.py
+++ demo.py
@@ -1,22 +1,23 @@
 import cv2
+import matplotlib.pyplot as plt
 import torch
 from PIL import Image
 from torchvision import transforms
 
-from datasets.freihand.data_utils import draw_2d_skeleton
+from datasets.freihand.data_utils import draw_2d_skeleton, draw_3d_skeleton_on_ax
 from datasets.freihand.dataset_utils import heatmaps_to_coordinates
-from models.blazenet_model_v4 import Pose2dModel
+from models.blazenet_model_3d_v4 import Pose3dModel
 
 config = {
     "model": {
         "n_keypoints": 21,
         "model_file": "exp/model_1.pth",
-        "device": "cpu",
+        "device": "cuda",
         "raw_image_size": 256,
         "model_img_size": 64,
     }
 }
-model = Pose2dModel(config)
+model = Pose3dModel(config)
 model = model.to(config["model"]["device"])
 model.load_state_dict(
     torch.load(config["model"]["model_file"], map_location=torch.device(config["model"]["device"]),)
@@ -28,6 +29,8 @@ image_transform = transforms.Compose(
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
+fig = plt.figure(figsize=(5, 5))
+ax = plt.axes(projection="3d")
 
 with torch.no_grad():
     while True:
@@ -43,18 +46,23 @@ with torch.no_grad():
 
         image_inp = image_transform(frame)
         image_inp = image_inp.unsqueeze(0)
-        image_inp = image_inp.float()
-        heatmaps_pred = model(image_inp)
-        heatmaps_pred = heatmaps_pred[0].numpy()[0]
+        image_inp = image_inp.float().to(config["model"]["device"])
+        pred = model(image_inp)
+        heatmaps_pred = pred[0].cpu().numpy()[0]
+        kpt_3d_pred = pred[1].cpu().numpy()[0]
 
         kpt_2d_pred = heatmaps_to_coordinates(heatmaps_pred, config["model"]["model_img_size"])
         kpt_2d_pred[:, 0] = kpt_2d_pred[:, 0] * im_width
         kpt_2d_pred[:, 1] = kpt_2d_pred[:, 1] * im_height
         skeleton_overlay = draw_2d_skeleton(frame, kpt_2d_pred)
+        ax.clear()
+        draw_3d_skeleton_on_ax(kpt_3d_pred, ax)
 
         skeleton_overlay = cv2.cvtColor(skeleton_overlay, cv2.COLOR_RGB2BGR)
         cv2.imshow("frame", skeleton_overlay)
 
+        plt.draw()
+        plt.pause(0.001)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
