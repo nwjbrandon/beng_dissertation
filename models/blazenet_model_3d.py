b801f5bfb80a718d97e20c813e48452f74f7ffe2
nwjbrandon <nwjbrandon@gmail.com>
2022-02-14 01:53:23 +0800

    add basic blocks in 3d

--- models/blazenet_model_3d.py
+++ models/blazenet_model_3d.py
@@ -1,63 +1,8 @@
 import torch
 from torch import nn
 
-from models.resnet import resnet18
-
-
-class Conv(nn.Module):
-    def __init__(
-        self, in_channels, out_channels, kernel_size, stride, padding,
-    ):
-        super(Conv, self).__init__()
-        self._conv1 = nn.Conv2d(
-            in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
-        )
-        self._act1 = nn.ReLU()
-
-    def forward(self, x):
-        x = self._conv1(x)
-        x = self._act1(x)
-        return x
-
-
-class ConvSig(nn.Module):
-    def __init__(
-        self, in_channels, out_channels, kernel_size, stride, padding,
-    ):
-        super(ConvSig, self).__init__()
-        self._conv1 = nn.Conv2d(
-            in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
-        )
-        self._act1 = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self._conv1(x)
-        x = self._act1(x)
-        return x
-
-
-class ConvBn(nn.Module):
-    def __init__(
-        self, in_channels, out_channels, kernel_size, stride, padding,
-    ):
-        super(ConvBn, self).__init__()
-        self._conv1 = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=False,
-        )
-        self._batch_norm1 = nn.BatchNorm2d(out_channels)
-        self._act1 = nn.ReLU()
-
-    def forward(self, x):
-        x = self._conv1(x)
-        x = self._batch_norm1(x)
-        x = self._act1(x)
-        return x
-
+from models.blazenet_model_v5 import Pose2dModel, ConvBn
+from models.resnet import BasicBlock
 
 class DownConv(nn.Module):
     def __init__(
@@ -75,92 +20,40 @@ class DownConv(nn.Module):
         return x
 
 
-class UpConv(nn.Module):
-    def __init__(
-        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, is_upsample=True
-    ):
-        super(UpConv, self).__init__()
-        self.is_upsample = is_upsample
-        self._conv1 = ConvBn(
-            in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
-        )
-        if self.is_upsample:
-            self._up2 = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
-
-    def forward(self, x):
-        x = self._conv1(x)
-        if self.is_upsample:
-            x = self._up2(x)
-        return x
-
-
-class OutConv(nn.Module):
-    def __init__(
-        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1,
-    ):
-        super(OutConv, self).__init__()
-        self._conv1 = ConvSig(
-            in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
-        )
-
-    def forward(self, x):
-        x = self._conv1(x)
-        return x
-
-
-class Decoder(nn.Module):
-    def __init__(self, out_channels):
-        super(Decoder, self).__init__()
-        self.conv6 = UpConv(in_channels=512, out_channels=32)
-        self.conv7 = UpConv(in_channels=288, out_channels=32)
-        self.conv8 = UpConv(in_channels=160, out_channels=32)
-        self.conv9 = UpConv(in_channels=96, out_channels=32, is_upsample=False)
-        self.conv10 = OutConv(in_channels=32, out_channels=out_channels)
-
-    def forward(self, out2, out3, out4, out5):
-        out6 = self.conv6(out5)
-        out7 = self.conv7(torch.cat([out6, out4], dim=1))
-        out8 = self.conv8(torch.cat([out7, out3], dim=1))
-        out9 = self.conv9(torch.cat([out8, out2], dim=1))
-        heatmaps = self.conv10(out9)
-        return heatmaps
-
-
-class Pose2dModel(nn.Module):
-    def __init__(self, config):
-        super(Pose2dModel, self).__init__()
-        self.out_channels = config["model"]["n_keypoints"]
-        self.resnet = resnet18()
-        self.decoder = Decoder(self.out_channels)
-
-    def forward(self, x):
-        out2, out3, out4, out5 = self.resnet(x)
-        heatmaps = self.decoder(out2, out3, out4, out5)
-        return heatmaps, out2, out3, out4, out5
-
-
 class Regressor3d(nn.Module):
     def __init__(self, config):
         super(Regressor3d, self).__init__()
         self.out_channels = config["model"]["n_keypoints"]
-        self.conv11 = DownConv(85, 32)
-        self.conv12 = DownConv(160, 64)
-        self.conv13 = DownConv(320, 192)
-        self.conv14 = DownConv(704, 192)
+        self.conv11 = DownConv(21, 21)
+        self.conv12 = BasicBlock(85, 85)
+        self.conv13 = DownConv(85, 32)
+        self.conv14 = BasicBlock(160, 160)
+        self.conv15 = DownConv(160, 64)
+        self.conv16 = BasicBlock(320, 320)
+        self.conv17 = DownConv(320, 192)
+        self.conv18 = BasicBlock(704, 704)
+        self.conv19 = DownConv(704, 192)
+
         self.flat = nn.Flatten()
         self.fc = nn.Linear(3072, self.out_channels * 3)
 
     def forward(self, heatmaps, out2, out3, out4, out5):
-        out11 = self.conv11(torch.cat([heatmaps, out2], dim=1))
-        out12 = self.conv12(torch.cat([out11, out3], dim=1))
-        out13 = self.conv13(torch.cat([out12, out4], dim=1))
-        out14 = self.conv14(torch.cat([out13, out5], dim=1))
-        x = self.flat(out14)
+        out11 = self.conv11(heatmaps)
+        out12 = self.conv12(torch.cat([out11, out2], dim=1))
+        out13 = self.conv13(out12)
+        out14 = self.conv14(torch.cat([out13, out3], dim=1))
+        out15 = self.conv15(out14)
+        out16 = self.conv16(torch.cat([out15, out4], dim=1))
+        out17 = self.conv17(out16)
+        out18 = self.conv18(torch.cat([out17, out5], dim=1))
+        out19 = self.conv19(out18)
+
+        x = self.flat(out19)
         kpt_3d = self.fc(x)
 
         # reshape
         B, _ = kpt_3d.shape
-        kpt_3d = kpt_3d.reshape((B, self.out_channels, -1))
+        kpt_3d = kpt_3d.view((B, self.out_channels, -1))
         return kpt_3d
 
 
